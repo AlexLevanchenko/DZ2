cd137b7a69f0cbb3c7f15188924d8a007aedbd8b
AlexLevanchenko <alex.levanchenko@gmail.com>
2022-04-14 23:31:17 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte x1 = 1;
+        int x2 = x1;
+        short x3 = x1;
+        long x4 = x1;
+
+        int y1 = 2;
+        double y2 = y1;
+
+        short z1 = 3;
+        float z2 = z1;
+        double z3 = z1;
+
+        char q1 = 9;
+        int q2 = q1;
+    }
+}
